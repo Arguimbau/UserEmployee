9d063df229abf2bb9fb590eeb1c9ccf86df8a333
ArthurOS <79909701+faxenarkoman@users.noreply.github.com>
2023-08-25 09:33:56 +0200

    Start project

---
+++ src/test/java/kea/dk/useremployee/UserEmployeeApplicationTests.java
@@ -0,0 +1,15 @@
+package kea.dk.useremployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEmployeeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
